b3fe95b747dad463de747b32b1d785b2e3cf850c
Yash Shirsath (GPU) <yash.shirsath@gmail.com>
2025-04-29 16:45:41 +0000

    analyze existing sae

--- src/sae/load_analyse_existing.py
+++ src/sae/load_analyse_existing.py
@@ -4,6 +4,8 @@ import os
 import torch
 from tqdm import tqdm
 import plotly.express as px
+import importlib
+import sys
 
 # Imports for displaying vis in Colab / notebook
 import webbrowser
@@ -35,6 +37,26 @@ def display_vis_inline(filename: str, height: int = 850):
         webbrowser.open(filename)
 
 
+# %%
+def reload_package(package_name):
+    """
+    Reload a package that has been newly installed without restarting the kernel.
+    
+    Args:
+        package_name (str): Name of the package to reload
+        
+    Returns:
+        The reloaded module
+    """
+    if package_name in sys.modules:
+        print(f"Reloading {package_name}...")
+        return importlib.reload(sys.modules[package_name])
+    else:
+        print(f"Importing {package_name}...")
+        return importlib.import_module(package_name)
+
+# Example usage:
+# reload_package('your_newly_installed_package')
 # %%
 from datasets import load_dataset
 from transformer_lens import HookedTransformer
@@ -101,4 +123,121 @@ with torch.no_grad():
     print("average l0", l0.mean().item())
     px.histogram(l0.flatten().cpu().numpy()).show()
 
-j  # %%
+# %%
+from transformer_lens import utils
+from functools import partial
+
+
+# next we want to do a reconstruction test.
+def reconstr_hook(activation, hook, sae_out):
+    return sae_out
+
+
+def zero_abl_hook(activation, hook):
+    return torch.zeros_like(activation)
+
+
+print("Orig", model(batch_tokens, return_type="loss").item())
+print(
+    "reconstr",
+    model.run_with_hooks(
+        batch_tokens,
+        fwd_hooks=[
+            (
+                sae.cfg.hook_name,
+                partial(reconstr_hook, sae_out=sae_out),
+            )
+        ],
+        return_type="loss",
+    ).item(),
+)
+print(
+    "Zero",
+    model.run_with_hooks(
+        batch_tokens,
+        return_type="loss",
+        fwd_hooks=[(sae.cfg.hook_name, zero_abl_hook)],
+    ).item(),
+)
+# %%
+example_prompt = "When John and Mary went to the shops, John gave the bag to"
+example_answer = " Mary"
+utils.test_prompt(example_prompt, example_answer, model, prepend_bos=True)
+
+logits, cache = model.run_with_cache(example_prompt, prepend_bos=True)
+tokens = model.to_tokens(example_prompt)
+sae_out = sae(cache[sae.cfg.hook_name])
+
+
+def reconstr_hook(activations, hook, sae_out):
+    return sae_out
+
+
+def zero_abl_hook(mlp_out, hook):
+    return torch.zeros_like(mlp_out)
+
+
+hook_name = sae.cfg.hook_name
+
+print("Orig", model(tokens, return_type="loss").item())
+print(
+    "reconstr",
+    model.run_with_hooks(
+        tokens,
+        fwd_hooks=[
+            (
+                hook_name,
+                partial(reconstr_hook, sae_out=sae_out),
+            )
+        ],
+        return_type="loss",
+    ).item(),
+)
+print(
+    "Zero",
+    model.run_with_hooks(
+        tokens,
+        return_type="loss",
+        fwd_hooks=[(hook_name, zero_abl_hook)],
+    ).item(),
+)
+
+
+with model.hooks(
+    fwd_hooks=[
+        (
+            hook_name,
+            partial(reconstr_hook, sae_out=sae_out),
+        )
+    ]
+):
+    utils.test_prompt(example_prompt, example_answer, model, prepend_bos=True)
+# %%
+from sae_dashboard.sae_vis_data import SaeVisConfig
+from sae_dashboard.sae_vis_runner import SaeVisRunner
+
+test_feature_idx_gpt = list(range(10)) + [14057]
+
+feature_vis_config_gpt = SaeVisConfig(
+    hook_point=hook_name,
+    features=test_feature_idx_gpt,
+    minibatch_size_features=64,
+    minibatch_size_tokens=256,
+    verbose=True,
+    device=device,
+)
+
+visualization_data_gpt = SaeVisRunner(
+    feature_vis_config_gpt
+).run(
+    encoder=sae,  # type: ignore
+    model=model,
+    tokens=token_dataset[:10000]["tokens"],  # type: ignore
+)
+# SaeVisData.create(
+#     encoder=sae,
+#     model=model, # type: ignore
+#     tokens=token_dataset[:10000]["tokens"],  # type: ignore
+#     cfg=feature_vis_config_gpt,
+# )
+# %%
