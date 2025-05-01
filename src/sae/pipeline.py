58223669aff81b8742d734f614ab25aea04cf4a8
Yash Shirsath (GPU) <yash.shirsath@gmail.com>
2025-05-01 14:04:55 +0000

    experimenting with pyproject and uv lock file

--- src/sae/pipeline.py
+++ src/sae/pipeline.py
@@ -1,15 +1,24 @@
 # %%
 from diffusers import StableDiffusionPipeline
-
+from IPython.display import Image, display
+import torch
 # %%
 
 
 def run(prompt: str, pipeline: StableDiffusionPipeline):
     print(prompt)
+    image = pipeline(prompt)
+    display(image.images[0])
 
 
 if __name__ == "__main__":
     pipeline = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
-    run("a photo of an astronaut riding a horse on mars", pipeline)
+    device = "cuda"
+    pipeline.to(device)
+    run("a portrait of a wise old warrior", pipeline)
+
+# %%
+print(torch.cuda.is_available())
+print(torch.cuda.get_arch_list())
 
 # %%
