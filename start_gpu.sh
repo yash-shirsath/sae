e5bb2a582169dac892d40f4b6f962341d38bb2bc
Yash Shirsath (GPU) <yash.shirsath@gmail.com>
2025-05-01 15:46:09 +0000

    cleaned up start_gpu.sh

--- start_gpu.sh
+++ start_gpu.sh
@@ -5,21 +5,17 @@ git clone https://${GITHUB_TOKEN}@github.com/yash-shirsath/sae.git
 git config --global user.email "yash.shirsath@gmail.com"
 git config --global user.name "Yash Shirsath (GPU)"
 
-
 touch ~/.no_auto_tmux
-pip uninstall torch
-pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
-
-# in parallel
-sudo apt install build-essential
 
 cursor --install-extension ms-python.python
+cursor --install-extension ms-toolsai.jupyter
 cursor --install-extension nvidia.nsight-vscode-edition
 
-
-pip install transformers datasets jaxtyping wandb
-pip install -e .
-cd steering/ 
+pip install uv
+uv venv
+source .venv/bin/activate
+pip install uv
+uv sync
 
 wandb login ${WANDB_API_KEY}
 
@@ -27,11 +23,18 @@ wandb login ${WANDB_API_KEY}
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
-
 gcloud init
-cd '/root/gradient-routing/steering/data/fineweb-edu'
-gsutil -m cp gs://gradient-routing/*.bin .
+
 
 
 # Materialize the script with actual tokens
 source .env && envsubst '${GITHUB_TOKEN} ${WANDB_API_KEY}' < start_gpu.sh > start_gpu_materialized.sh
+
+
+# archive
+pip uninstall torch
+uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+
+sudo apt install build-essential
+
+gsutil -m cp gs://gradient-routing/*.bin .
