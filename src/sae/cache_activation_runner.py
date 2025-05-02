dc09f398e463a02b69710ff370add71bab753972
Yash Shirsath (GPU) <yash.shirsath@gmail.com>
2025-05-02 21:32:20 +0000

    finished run_with_cache

--- src/sae/cache_activation_runner.py
+++ src/sae/cache_activation_runner.py
@@ -1,4 +1,5 @@
-from typing import Union, List, Optional
+from collections import defaultdict
+from typing import Union, List, Optional, Tuple
 from diffusers import StableDiffusionPipeline  # type: ignore
 from dataclasses import dataclass
 import torch as t
@@ -19,6 +20,9 @@ class CacheActivationsRunner:
         takes pipeline and dataset, runs stable diffusion
         """
         pipe = HookedDiffusionPipeline.from_pretrained(self.cfg.model_name)
+        pipe.to(self.cfg.device)
+
+        pipe.run_with_cache("Sick image of clouds")
 
 
 class HookedDiffusionPipeline:
@@ -32,8 +36,8 @@ class HookedDiffusionPipeline:
         guidance_scale: float = 7.5,
         num_inference_steps: int = 50,
         generator: Optional[Union[t.Generator, List[t.Generator]]] = None,
-    ):
-        cache_input, cache_output = dict(), dict()
+    ) -> Tuple[dict, dict]:
+        cache_input, cache_output = defaultdict(list), defaultdict(list)
         positions_to_cache = ["unet.up_blocks.1.attentions.1"]
 
         hook_handles = [
@@ -42,23 +46,39 @@ class HookedDiffusionPipeline:
         ]
         hook_handles = [h for h in hook_handles if h]
 
-        self._run_generation_pipeline()
+        self._run_generation_pipeline(
+            prompt, guidance_scale, num_inference_steps, generator
+        )
 
         for handle in hook_handles:
             handle.remove()
 
         return cache_input, cache_output
 
+    def _run_generation_pipeline(
+        self,
+        prompt: Union[str, List[str]],
+        guidance_scale: float,
+        num_inference_steps: int,
+        generator: Optional[Union[t.Generator, List[t.Generator]]],
+    ):
+        self.pipe(
+            prompt,
+            guidance_scale=guidance_scale,
+            num_inference_steps=num_inference_steps,
+            generator=generator,
+        )
+
     def _register_cache(self, position: str, cache_input: dict, cache_output: dict):
         def hook_fn(module, input, output):
-            cache_input[position] = input
-            cache_output[position] = output
+            cache_input[position].append(input)
+            cache_output[position].append(output)
 
         block: t.nn.Module = self._locate_block(position)
-        return block.register_forward_pre_hook(hook_fn)
+        return block.register_forward_hook(hook_fn)
 
     def _locate_block(self, position: str):
-        block = self.pipe.unet
+        block = self.pipe
         for step in position.split("."):
             if step.isdigit():
                 step = int(step)
@@ -67,8 +87,17 @@ class HookedDiffusionPipeline:
                 block = getattr(block, step)
         return block
 
+    def to(self, device: str):
+        self.pipe.to(device)
+
     @classmethod
     def from_pretrained(cls, model_name: str) -> "HookedDiffusionPipeline":
         pipe = StableDiffusionPipeline.from_pretrained(model_name)
         assert isinstance(pipe, StableDiffusionPipeline)
         return cls(pipe)
+
+
+if __name__ == "__main__":
+    cfg = CacheActivationsRunnerCfg()
+    runner = CacheActivationsRunner(cfg)
+    runner.run()
