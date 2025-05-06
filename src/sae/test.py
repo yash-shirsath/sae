c15db1c975f8abc7c67e8bc6af577c743a52cce2
Yash S <shirsath.yash@gmail.com>
2025-05-06 10:39:15 -0400

    added sae model files

--- src/sae/test.py
+++ src/sae/test.py
@@ -1,7 +1,25 @@
-#%% 
+# %%
 import torch
 
 # %%
 print(torch.__version__)
+
+
+# %%
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# %%
+a = TreeNode(1)
+a.__dict__["parent"] = TreeNode(2)
+
+if "parent" in a.__dict__:
+    print("cool")
+else:
+    print("a.left is not TreeNode")
 
 # %%
